7c3c4094761d849a0f7ef824c1cfdcd2026531ea
Harrison Schroeder <harrison.schroeder@kellogg.northwestern.edu>
2025-04-22 14:31:53 +0000

    Exercises 1 and 2

--- code-along/2-conditionals.rb
+++ code-along/2-conditionals.rb
@@ -3,13 +3,38 @@
 # ruby 2-conditionals.rb
 
 # Booleans
+true
+false
 
 # Boolean Expressions
+x = 3
+y = 5 
+puts x == y
+puts x != y
 
 # If Conditional Logic
-
+if x != y 
+    puts "We have not achieved equality"
+end 
+if x == y 
+    puts "We have achieved equality"
+end 
 # If/Else Conditional Logic
+if x == y
+    puts "we have achieved equality"
+elsif x > y
+    puts "we have achieved greater than"
+else
+    puts "we have achieved lesser than"
+end
 
 # Elsif Conditional Logic
 
 # Combining Expressions
+if x == y || x < y
+    puts "less than or equal to"
+end
+
+if x != y && x < y
+    puts "strictly less than"
+end
