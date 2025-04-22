2cf6322972c5200927fb3269d8f254cb6245b313
Harrison Schroeder <harrison.schroeder@kellogg.northwestern.edu>
2025-04-22 14:32:04 +0000

    Exercises 1 and 2

--- code-along/1-data.rb
+++ code-along/1-data.rb
@@ -5,15 +5,34 @@
 # In Ruby, there are different *types* of data:
 
 # Numbers
+puts 3
+puts 3/7.0
 
 # Non-numbers
+puts "gitpod"
+puts "3/5"
 
 # True, False
+puts true
+puts false
 
 # Nothing
+puts nil
 
 # Variables
+x = 20
+y = 5
+amount = x + y
+food = "tacos"
+puts x/y
+puts x * y
+puts nil
 
 # Combine strings and variables
+puts food.class
+puts amount.class
+puts "We have #{amount} types of #{food} on today's menu"
 
 # String manipulation
+puts food.reverse
+puts food.upcase
